30f3fde05129a4e0cc025fb11f0c0548ef2897f2
Mark C Mitchell <mark@markcmitchell.net>
2016-07-20 14:11:34 +1000

    Refactor set-focus-only-timeout to implement UMD

--- set-focus-only-timeout.js
+++ set-focus-only-timeout.js
@@ -1,9 +1,23 @@
-'use strict';
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['ifvisible.js'], function (ifvisible) {
+            return (root.returnExportsGlobal = factory(ifvisible));
+        });
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory(require('ifvisible.js'));
+    } else {
+        // Browser globals (root is window)
+        root.returnExportsGlobal = factory(root.ifvisible);
+    }
 
-(function(window) {
+}(this, function (ifvisible) {
+    'use strict';
 
     function focusOnlyTimeout(cb, timeout) {
-
         var started = new Date();
         var remaining = timeout;
 
@@ -27,9 +41,13 @@
             started = new Date();
             timeoutId = start();
         }
-        return timeoutId;
-    }
 
+        return timeoutId;
+    };
+
+    // place setFocusOnlyTimeout on window.
     window.setFocusOnlyTimeout = focusOnlyTimeout;
 
-})(window);
+    // Return a value to define the module export.
+    return focusOnlyTimeout;
+}));
